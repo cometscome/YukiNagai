188d345f3d6c331a52a890e848cfc3ea191f2945
cometscome <cometscome@gmail.com>
2022-07-19 17:43:53 +0900

    first commit

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Documenter.jl-を使ってウェブサイトを作成する","page":"Documenter.jl を使ってウェブサイトを作成する","title":"Documenter.jl を使ってウェブサイトを作成する","text":"","category":"section"},{"location":"","page":"Documenter.jl を使ってウェブサイトを作成する","title":"Documenter.jl を使ってウェブサイトを作成する","text":"hogehoge","category":"page"}]
+}
---
+++ docs/make.jl
@@ -0,0 +1,5 @@
+
+using Documenter
+makedocs(;
+    sitename="Demo"
+)
